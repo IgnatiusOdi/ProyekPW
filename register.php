14f850beb502a8fee1608943e9e192d22a265395
Ignatius Odi <ignodi@gmail.com>
2021-11-04 16:02:29 +0700

    edit

--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php
-
+    $user = mysqli_connect("localhost", "root", "");
+    
+    mysqli_close($user);
 ?>
 
 <!DOCTYPE html>
@@ -8,19 +10,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
 </head>
 <body>
     <form action="" method="post">
-        <h1>Login</h1>
+        <h1>Register</h1>
         <label for="username">Username</label>
         <input type="text" name="username" placeholder="Username">
         <br>
         <label for="password">Password</label>
         <input type="text" name="password" placeholder="Password">
         <br>
-        <input type="submit" name="login" value="Login">
-        <label>Don't have account? <a href="register.php">Register Now!</a></label>
+        <input type="submit" name="register" value="Register">
+        <label>Already have account? <a href="login.php">Let's Login!</a></label>
     </form>
 </body>
 </html>
