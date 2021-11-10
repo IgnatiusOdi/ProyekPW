f2277f3262bdb2d569bc5d967a0006f28991277e
jlairlangga <john_la@mhs.istts.ac.id>
2021-11-10 21:59:34 +0700

    home

--- php/home.php
+++ php/home.php
@@ -1,60 +1,167 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-        <meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Idk judul</title>
- 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
- 		<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css"/>
- 		<link type="text/css" rel="stylesheet" href="../css/slick.css"/>
- 		<link type="text/css" rel="stylesheet" href="../css/slick-theme.css"/>
- 		<link type="text/css" rel="stylesheet" href="../css/nouislider.min.css"/>
- 		<link rel="stylesheet" href="../css/font-awesome.min.css">
- 		<link type="text/css" rel="stylesheet" href="../css/style.css"/>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Idk judul</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="../css/slick.css" />
+	<link type="text/css" rel="stylesheet" href="../css/slick-theme.css" />
+	<link type="text/css" rel="stylesheet" href="../css/nouislider.min.css" />
+	<link rel="stylesheet" href="../css/font-awesome.min.css">
+	<link type="text/css" rel="stylesheet" href="../css/style.css" />
 </head>
+
 <body>
-    <div id="header">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-3">
-							<div class="header-logo">
-								<a href="#" class="logo">
-									<img src="../img/logo.jpg" alt="hai">
-								</a>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="header-search">
-								<form>
-									<select class="input-select">
-										<option value="0">All Categories</option>
-										<option value="1">Racket</option>
-										<option value="2">Clothes</option>
-										<option value="3">Shoes</option>
-										<option value="4">Gear</option>
-									</select>
-									<input class="input" placeholder="Search here">
-									<button class="search-btn">Search</button>
-								</form>
-							</div>
-						</div>
-						<div class="col-md-3 clearfix">
-							<button class="btn"><a href="login.php">Sign in</a></button>
-						</div>
+	<div id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="header-logo">
+						<a href="#" class="logo">
+							<img src="../img/logo.jpg" alt="hai">
+						</a>
 					</div>
 				</div>
+				<div class="col-md-6">
+					<div class="header-search">
+						<form>
+							<select class="input-select">
+								<option value="0">All Categories</option>
+								<option value="1">Racket</option>
+								<option value="2">Clothes</option>
+								<option value="3">Shoes</option>
+								<option value="4">Gear</option>
+							</select>
+							<input class="input" placeholder="Search here">
+							<button class="search-btn">Search</button>
+						</form>
+					</div>
+				</div>
+				<div class="col-md-3 clearfix">
+					<button class="btn"><a href="login.php">Sign in</a></button>
+				</div>
 			</div>
-		</header>
+		</div>
+	</div>
+	</header>
+
+	
+	<div class="section">
+		
+		<div class="container">
+		
+			<div class="row">
+			
+				<div class="col-md-12">
+					<div class="row">
+						<div class="products-tabs">
+						
+						
+							<div class="product">
+								<div class="product-img">
+									<img src="../img/logo.jpg" alt="" style="height: 345px; width: 345px;">
+								</div>
+								<div class="product-body">
+									<p class="product-category">Category</p>
+									<h3 class="product-name"><a href="#">Racket</a></h3>
+								
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+
+								</div>
+
+							</div>
+							<!-- /product -->
+							<div class="product">
+								<div class="product-img">
+									<img src="../img/logo.jpg" alt="" style="height: 345px; width: 345px;">
+								</div>
+								<div class="product-body">
+									<p class="product-category">Category</p>
+									<h3 class="product-name"><a href="#">Racket</a></h3>
+									<!-- <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4> -->
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+
+								</div>
+
+							</div>
+
+							<!-- /product -->
+							<div class="product">
+								<div class="product-img">
+									<img src="../img/logo.jpg" alt="" style="height: 345px; width: 345px;">
+								</div>
+								<div class="product-body">
+									<p class="product-category">Category</p>
+									<h3 class="product-name"><a href="#">Racket</a></h3>
+									<!-- <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4> -->
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+
+								</div>
+
+							</div>
+							<!-- product -->
+							<div class="product">
+								<div class="product-img">
+									<img src="../img/logo.jpg" alt="" style="height: 345px; width: 345px;">
+								</div>
+								<div class="product-body">
+									<p class="product-category">Category</p>
+									<h3 class="product-name"><a href="#">Racket</a></h3>
+									<!-- <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4> -->
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+
+								</div>
+
+							</div>
+							<!-- </div> -->
+						</div>
+						<!-- /tab -->
+					</div>
+				</div>
+				<!-- </div> -->
+				<!-- Products tab & slick -->
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
 
 
-        <script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/slick.min.js"></script>
-		<script src="js/nouislider.min.js"></script>
-		<script src="js/jquery.zoom.min.js"></script>
-		<script src="js/main.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/nouislider.min.js"></script>
+	<script src="js/jquery.zoom.min.js"></script>
+	<script src="js/main.js"></script>
 </body>
+
 </html>
