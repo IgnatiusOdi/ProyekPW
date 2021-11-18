3a89b7b907d3fddda973dfc351ce78af21bdd17c
jlairlangga <john_la@mhs.istts.ac.id>
2021-11-18 13:19:47 +0700

    Update home.php

--- php/home.php
+++ php/home.php
@@ -212,7 +212,7 @@
 						<div class="col-md-12 text-center">
 							<span class="copyright" style="color: white;">
 								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made by Colorlib
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 							</span>
 						</div>
