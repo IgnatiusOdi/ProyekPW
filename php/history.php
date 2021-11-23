9bc95cae9f5a7fa6a5e9fbe4922d41ad9a1ae944
jlairlangga <john_la@mhs.istts.ac.id>
2021-11-23 12:05:12 +0700

    history
    cmn bagian header nya aja

--- php/history.php
+++ php/history.php
@@ -5,6 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>History</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .header a {
+            justify-content: center;
+            margin-right: 10px;
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .header {
+            background-color: #333;
+            overflow: hidden;
+            display: flex;
+            position: sticky;
+            /* margin-left: 100px; */
+        }
+    </style>
 </head>
 <body>
     <div class="header">
