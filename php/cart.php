924e2b8bb39c81378eca44aa4c961abecd6990bb
Ignatius Odi <ignodi@gmail.com>
2021-11-23 23:48:08 +0700

    trying to fix things and update

--- php/cart.php
+++ php/cart.php
@@ -6,8 +6,18 @@
     }
 
     $idUser = $_SESSION['user'] + 1;
-    $listCart = $conn -> query("SELECT * FROM cart WHERE id_users='$idUser' AND kondisi='1'") -> fetch_all(MYSQLI_ASSOC);
+    $listCart = $conn -> query("SELECT * FROM cart WHERE id_users='$idUser'") -> fetch_all(MYSQLI_ASSOC);
     $hargaTotal = 0;
+
+    foreach ($listCart as $key => $value) {
+        if (isset($_REQUEST['delete-'.$value['id_cart']])) {
+            //RETURN STOK
+
+            //DELETE FROM CART
+
+
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +27,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cart</title>
+    <script src="../js/jquery.min.js"></script>
 </head>
 <body>
     <div class="header">
@@ -32,21 +43,39 @@
             <td>Harga</td>
             <td>Action</td>
         </tr>
-        <?php
-            foreach ($listCart as $key => $value) {
-                echo "<tr>";
-                    echo "<td>".($key + 1).".</td>";
-                    $barang = $listBarang[$value['id_barang'] - 1];
-                    echo "<td><img src=".$barang['foto_barang']."><br>".$barang['nama_barang']."</td>";
-                    echo "<td>".$value['jumlah']."</td>";
-                    echo "<td>Rp. ".number_format($barang['harga_barang'],0,'','.').",-</td>";
-                    echo "<td><button name='delete-".$value['id_cart']."'>Cancel</button></td>";
-                echo "</tr>";
-                $hargaTotal += $value['jumlah'] * $barang['harga_barang'];
-            }
-        ?>
+        <div class="cart">
+            <?php
+                foreach ($listCart as $key => $value) {
+                    echo "<tr>";
+                        echo "<td>".($key + 1).".</td>";
+                        $barang = $listBarang[$value['id_barang'] - 1];
+                        echo "<td><img src=".$barang['foto_barang']."><br>".$barang['nama_barang']."</td>";
+                        echo "<td>".$value['jumlah']."</td>";
+                        echo "<td>Rp. ".number_format($barang['harga_barang'],0,'','.').",-</td>";
+                        echo "<td><button name='delete-".$value['id_cart']."'>Cancel</button></td>";
+                    echo "</tr>";
+                    $hargaTotal += $value['jumlah'] * $barang['harga_barang'];
+                }
+            ?>
+        </div>
     </table>
     <h3>Total: Rp. <?=number_format($hargaTotal,0,'','.')?>,-</h3>
     <button>Checkout</button>
+
+    <script>
+        function delete(id) {
+            $.ajax({
+                type:"get",
+                url:"./controller.php",
+                data:{
+                    'action':'deleteFromCart',
+                    'id_cart':id
+                },
+                success:function(response){
+                    $(".cart").html(response);
+                }
+            });
+        }
+    </script>
 </body>
 </html>
