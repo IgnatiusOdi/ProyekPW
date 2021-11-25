2ee217895e73a97413488cbfb7cf2266a7bdaa71
jlairlangga <john_la@mhs.istts.ac.id>
2021-11-25 14:12:16 +0700

    header

--- php/barang.php
+++ php/barang.php
@@ -37,6 +37,7 @@
     <link type="text/css" rel="stylesheet" href="../css/slick-theme.css" />
     <link type="text/css" rel="stylesheet" href="../css/nouislider.min.css" />
     <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="../css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
     <script src="../js/jquery.min.js"></script>
 </head>
--- php/history.php
+++ php/history.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>History</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../css/style.css" />
     <style>
         body{
             margin: 0;
